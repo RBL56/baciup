ea8c0fd7e3978005534e88e269193eb0a8e8e34b
RBL56 <Rabsterloco56@gmail.com>
2026-02-07 17:50:44 +0300

    Fix syntax error in Purchase.js braces

--- src/external/bot-skeleton/services/tradeEngine/trade/Purchase.js
+++ src/external/bot-skeleton/services/tradeEngine/trade/Purchase.js
@@ -173,118 +173,116 @@ export default Engine =>
                                         // 6. Notify UI
                                         const account_type = loginid.startsWith('CR') ? 'Real' : 'Demo';
                                         console.log(`[VH] Switched to ${account_type} (${loginid}). Mode: ${new_mode}`);
+                                        // 7. Refresh Proposals for new account
+                                        // We need a fresh proposal ID for the new account to avoid InvalidContractProposal errors
+                                        this.renewProposalsOnPurchase();
+
+                                        // Wait for proposals to be ready
+                                        await new Promise(resolve => {
+                                            const check = () => {
+                                                if (this.store.getState().proposalsReady) {
+                                                    resolve();
+                                                } else {
+                                                    setTimeout(check, 200);
+                                                }
+                                            };
+                                            check();
+                                        });
+
+                                        // Select new proposal
+                                        const { id: newId, askPrice: newAskPrice } = this.selectProposal(contract_type);
+
+                                        // Execute trade with NEW proposal
+                                        return api_base.api.send({ buy: newId, price: newAskPrice });
                                     }
                                 }
-                                // 7. Refresh Proposals for new account
-                                // We need a fresh proposal ID for the new account to avoid InvalidContractProposal errors
-                                this.renewProposalsOnPurchase();
-
-                                // Wait for proposals to be ready
-                                await new Promise(resolve => {
-                                    const check = () => {
-                                        if (this.store.getState().proposalsReady) {
-                                            resolve();
-                                        } else {
-                                            setTimeout(check, 200);
-                                        }
-                                    };
-                                    check();
-                                });
-
-                                // Select new proposal
-                                const { id: newId, askPrice: newAskPrice } = this.selectProposal(contract_type);
-
-                                // Execute trade with NEW proposal
-                                return api_base.api.send({ buy: newId, price: newAskPrice });
                             }
                         }
+                    } catch (e) {
+                        console.error('[VH] Error in Virtual Hook logic:', e);
                     }
-                        }
-            } catch (e) {
-                console.error('[VH] Error in Virtual Hook logic:', e);
-            }
-            // VIRTUAL HOOK LOGIC END
+                    // VIRTUAL HOOK LOGIC END
 
-            return api_base.api.send({ buy: id, price: askPrice });
-        };
+                    return api_base.api.send({ buy: id, price: askPrice });
+                };
 
                 this.isSold = false;
 
-contractStatus({
-    id: 'contract.purchase_sent',
-    data: askPrice,
-});
+                contractStatus({
+                    id: 'contract.purchase_sent',
+                    data: askPrice,
+                });
 
-if (!this.options.timeMachineEnabled) {
-    return doUntilDone(action).then(onSuccess).then(() => {
-        // UPDATE VH INVALID STATE ON COMPLETION
-        const DBotStore = require('../../../scratch/dbot-store').default;
-        const { client } = DBotStore.instance || {};
-        const vh_settings = client?.virtual_hook_settings;
+                if (!this.options.timeMachineEnabled) {
+                    return doUntilDone(action).then(onSuccess).then(() => {
+                        // UPDATE VH INVALID STATE ON COMPLETION
+                        const DBotStore = require('../../../scratch/dbot-store').default;
+                        const { client } = DBotStore.instance || {};
+                        const vh_settings = client?.virtual_hook_settings;
 
-        // We check contract result
-        // The `onSuccess` callback receives `response`.
-        // But here we are IN the promise chain.
-        // We need to capture the result.
-    });
-}
+                        // We check contract result
+                        // The `onSuccess` callback receives `response`.
+                        // But here we are IN the promise chain.
+                        // We need to capture the result.
+                    });
+                }
 
-return recoverFromError(
-    action,
-    (errorCode, makeDelay) => {
-        // if disconnected no need to resubscription (handled by live-api)
-        if (errorCode !== 'DisconnectError') {
-            this.renewProposalsOnPurchase();
-        } else {
-            this.clearProposals();
-        }
+                return recoverFromError(
+                    action,
+                    (errorCode, makeDelay) => {
+                        // if disconnected no need to resubscription (handled by live-api)
+                        if (errorCode !== 'DisconnectError') {
+                            this.renewProposalsOnPurchase();
+                        } else {
+                            this.clearProposals();
+                        }
 
-        const unsubscribe = this.store.subscribe(() => {
-            const { scope, proposalsReady } = this.store.getState();
-            if (scope === BEFORE_PURCHASE && proposalsReady) {
-                makeDelay().then(() => this.observer.emit('REVERT', 'before'));
-                unsubscribe();
+                        const unsubscribe = this.store.subscribe(() => {
+                            const { scope, proposalsReady } = this.store.getState();
+                            if (scope === BEFORE_PURCHASE && proposalsReady) {
+                                makeDelay().then(() => this.observer.emit('REVERT', 'before'));
+                                unsubscribe();
+                            }
+                        });
+                    },
+                    ['PriceMoved', 'InvalidContractProposal'],
+                    delayIndex++
+                ).then(onSuccess);
             }
-        });
-    },
-    ['PriceMoved', 'InvalidContractProposal'],
-    delayIndex++
-).then(onSuccess);
+            const trade_option = tradeOptionToBuy(contract_type, this.tradeOptions);
+            const action = () => api_base.api.send(trade_option);
+
+            this.isSold = false;
+
+            contractStatus({
+                id: 'contract.purchase_sent',
+                data: this.tradeOptions.amount,
+            });
+
+            if (!this.options.timeMachineEnabled) {
+                return doUntilDone(action).then(onSuccess);
             }
-const trade_option = tradeOptionToBuy(contract_type, this.tradeOptions);
-const action = () => api_base.api.send(trade_option);
 
-this.isSold = false;
-
-contractStatus({
-    id: 'contract.purchase_sent',
-    data: this.tradeOptions.amount,
-});
-
-if (!this.options.timeMachineEnabled) {
-    return doUntilDone(action).then(onSuccess);
-}
-
-return recoverFromError(
-    action,
-    (errorCode, makeDelay) => {
-        if (errorCode === 'DisconnectError') {
-            this.clearProposals();
+            return recoverFromError(
+                action,
+                (errorCode, makeDelay) => {
+                    if (errorCode === 'DisconnectError') {
+                        this.clearProposals();
+                    }
+                    const unsubscribe = this.store.subscribe(() => {
+                        const { scope } = this.store.getState();
+                        if (scope === BEFORE_PURCHASE) {
+                            makeDelay().then(() => this.observer.emit('REVERT', 'before'));
+                            unsubscribe();
+                        }
+                    });
+                },
+                ['PriceMoved', 'InvalidContractProposal'],
+                delayIndex++
+            ).then(onSuccess);
         }
-        const unsubscribe = this.store.subscribe(() => {
-            const { scope } = this.store.getState();
-            if (scope === BEFORE_PURCHASE) {
-                makeDelay().then(() => this.observer.emit('REVERT', 'before'));
-                unsubscribe();
-            }
-        });
-    },
-    ['PriceMoved', 'InvalidContractProposal'],
-    delayIndex++
-).then(onSuccess);
-        }
-getPurchaseReference = () => purchase_reference;
-regeneratePurchaseReference = () => {
-    purchase_reference = getUUID();
-};
+        getPurchaseReference = () => purchase_reference;
+        regeneratePurchaseReference = () => {
+            purchase_reference = getUUID();
+        };
     };
